cd56f43ba91061bd81fe76378423d3009ce2ac8d
jonathan-saunders <jonsaunders43@gmail.com>
2024-07-22 10:27:55 +0100

    Complete exercise on passing pointers to functions

---
+++ Chapter12-Pointers-and-References/passPtrtoFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void multiplyPtr(int *ptr, int multiplier) {
+    *ptr *= multiplier;
+}
+
+
+int main() {
+    
+    int *ptr {nullptr};
+    int num { 12 };
+    
+    std::cout << &ptr << std::endl;
+    ptr = &num;
+    std::cout << *ptr << std::endl;
+    
+    multiplyPtr(ptr, 3);
+    
+    std::cout << *ptr;
+    
+    return 0;
+}
