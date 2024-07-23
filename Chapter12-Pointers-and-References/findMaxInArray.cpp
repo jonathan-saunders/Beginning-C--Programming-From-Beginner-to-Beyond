6d1aa1cab731708f35ccdc37ee73bb90cc8a28d1
jonathan-saunders <jonsaunders43@gmail.com>
2024-07-23 20:06:43 +0100

    Add comments for the find max element using pointers function

--- Chapter12-Pointers-and-References/findMaxInArray.cpp
+++ Chapter12-Pointers-and-References/findMaxInArray.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+/*
+    Declares a function that takes a pointer to an integer, and an integer called size
+    The 'largest' integer is set to the first item in the array at index 0
+    The for loop, starting at index [1], iterates through the array
+    The if statement checks whether the item at index [i] is greater than largest
+    If it is, then the item at item [i] becomes the new largest.
+    Once the for loop has completed, largest is returned to the function caller.
+
+*/
+
 int find_max_element(int* arr, int size) {
     int largest = arr[0]; // Initialize to the first element in the array
    
